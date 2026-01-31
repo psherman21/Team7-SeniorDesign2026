37e8b9d1b32b3b7a1b24c0d54c3f22a60e43c37b
Philip <pls35@pitt.edu>
2026-01-31 15:19:43 -0500

    added initial data_logger file.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Flex Glove Gesture Recognition (ML)
 
 This repository contains the machine learning and data processing components Team 7's Senior Design Project
+
+# Input Data
+
+Input Data will be read in this format and monitored at a rate of 20Hz:
+
+thumb,index,middle,ring,pinky,label
+32,45,78,80,60,A
---
+++ scripts/data_logger.py
@@ -0,0 +1,36 @@
+import serial
+import csv
+import time
+from datetime import datetime
+
+SERIAL_PORT = "COM3" # Adjust when needed
+BAUD_RATE = 115200
+GESTURE_LABEL = "A" # Change with every recording
+OUTPUT_FILE = f"data/raw/gesture_{GESTURE_LABEL}.csv"
+
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+time.sleep(2)
+
+print(f"Recording gesture '{GESTURE_LABEL}'")
+print("Press Ctrl+C to stop recording\n")
+
+with open(OUTPUT_FILE, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["timestamp", "thumb", "index", "middle", "ring", "pinky", "label"])
+    
+    try:
+        while True:
+            line = ser.readline().decode("utf-8").strip()
+            values = line.split(",")
+
+            if len(values) != 5:
+                continue
+
+            timestamp = datetime.now().isoformat()
+            writer.writerow([timestamp, *values, GESTURE_LABEL])
+            print(values)
+
+    except KeyboardInterrupt:
+        print("\nRecording stopped.")
+
+ser.close()
